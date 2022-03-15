90b21f59cc30cf340987ab0375b5b0094f981dd8
Vicente Aranda <vicente.aranda@protonmail.com>
2022-03-15 16:53:46 -0300

    Created Indicators filling function

--- src/Components/Board.tsx
+++ src/Components/Board.tsx
@@ -25,16 +25,105 @@ const fillBombs = (cells: string[], bombs: number):string[] => {
     return cells;
 };
 
+const fillIndicators = (cells: string[], rows: number): string[] => {
+    
+    for (let i = 0, n = cells.length; i < n; i++) {
+        if (cells[i] !== '💣') continue;
+        // Edge cases: top-left, top-right, bottom-left, bottom-right => 3 available cells
+        let topLeft = i-rows-1,
+        top = i-rows,
+        topRight = i-rows+1,
+        left = i-1,
+        right = i+1,
+        bottomLeft = i+rows-1,
+        bottom = i+rows,
+        bottomRight = i+rows+1;
+        switch (i) {
+            case 0:
+                // Top-left
+                if (cells[right] !== '💣') cells[right] = String(Number(cells[right])+1);
+                if (cells[bottomRight] !== '💣') cells[bottomRight] = String(Number(cells[bottomRight])+1);
+                if (cells[bottom] !== '💣') cells[bottom] = String(Number(cells[bottom])+1);
+                break;
+                case rows-1:    
+                // Top-right
+                if (cells[bottom] !== '💣') cells[bottom] = String(Number(cells[bottom])+1);
+                if (cells[bottomLeft] !== '💣') cells[bottomLeft] = String(Number(cells[bottomLeft])+1);
+                if (cells[left] !== '💣') cells[left] = String(Number(cells[left])+1);
+                break;
+            case rows*(rows-1):
+                // Bottom-left
+                if (cells[top] !== '💣') cells[top] = String(Number(cells[top])+1);
+                if (cells[topRight] !== '💣') cells[topRight] = String(Number(cells[topRight])+1);
+                if (cells[right] !== '💣') cells[right] = String(Number(cells[right])+1);
+                break;
+            case rows*rows-1:
+                // Bottom-right
+                if (cells[left] !== '💣') cells[right] = String(Number(cells[right])+1);
+                if (cells[topLeft] !== '💣') cells[topLeft] = String(Number(cells[topLeft])+1);
+                if (cells[top] !== '💣') cells[top] = String(Number(cells[top])+1);
+                break;
+            default:
+                break;
+        }
+
+        // Other special cases: top, bottom, left, right => 5 available cells
+        if (i > 0 && i < rows-1) {
+            // Top Center
+            if (cells[right] !== '💣') cells[right] = String(Number(cells[right])+1);
+            if (cells[bottomRight] !== '💣') cells[bottomRight] = String(Number(cells[bottomRight])+1);
+            if (cells[bottom] !== '💣') cells[bottom] = String(Number(cells[bottom])+1);
+            if (cells[bottomLeft] !== '💣') cells[bottomLeft] = String(Number(cells[bottomLeft])+1);
+            if (cells[left] !== '💣') cells[left] = String(Number(cells[left])+1);
+        }
+        else if (i > rows*(rows-1) && i < (rows*rows)-1) {
+            // Bottom Center
+            if (cells[top] !== '💣') cells[top] = String(Number(cells[top])+1);
+            if (cells[topRight] !== '💣') cells[topRight] = String(Number(cells[topRight])+1);
+            if (cells[right] !== '💣') cells[right] = String(Number(cells[right])+1);
+            if (cells[left] !== '💣') cells[left] = String(Number(cells[left])+1);
+            if (cells[topLeft] !== '💣') cells[topLeft] = String(Number(cells[topLeft])+1);
+        }
+        else if (i % rows === 0 && i !== 0 && i !== rows*(rows-1)) {
+            // Left Center
+            if (cells[top] !== '💣') cells[top] = String(Number(cells[top])+1);
+            if (cells[topRight] !== '💣') cells[topRight] = String(Number(cells[topRight])+1);
+            if (cells[right] !== '💣') cells[right] = String(Number(cells[right])+1);
+            if (cells[bottomRight] !== '💣') cells[bottomRight] = String(Number(cells[bottomRight])+1);
+            if (cells[bottom] !== '💣') cells[bottom] = String(Number(cells[bottom])+1);
+        }
+        else if (i+1 % rows === 0 && i !== rows-1 && i !== (rows*rows)-1) {
+            // Left Center
+            if (cells[top] !== '💣') cells[top] = String(Number(cells[top])+1);
+            if (cells[bottom] !== '💣') cells[bottom] = String(Number(cells[bottom])+1);
+            if (cells[bottomLeft] !== '💣') cells[bottomLeft] = String(Number(cells[bottomLeft])+1);
+            if (cells[left] !== '💣') cells[left] = String(Number(cells[left])+1);
+            if (cells[topLeft] !== '💣') cells[right] = String(Number(cells[right])+1);
+        }
+        else {
+            // Center
+            if (cells[top] !== '💣') cells[top] = String(Number(cells[top])+1);
+            if (cells[topRight] !== '💣') cells[topRight] = String(Number(cells[topRight])+1);
+            if (cells[right] !== '💣') cells[right] = String(Number(cells[right])+1);
+            if (cells[bottomRight] !== '💣') cells[bottomRight] = String(Number(cells[bottomRight])+1);
+            if (cells[bottom] !== '💣') cells[bottom] = String(Number(cells[bottom])+1);
+            if (cells[bottomLeft] !== '💣') cells[bottomLeft] = String(Number(cells[bottomLeft])+1);
+            if (cells[left] !== '💣') cells[left] = String(Number(cells[left])+1);
+            if (cells[topLeft] !== '💣') cells[right] = String(Number(cells[right])+1);
+        }
+    }
+    return cells;
+}
+
 
 const Board = (props: BoardProps) => {
-    const cells: string[] = Array(props.rows*props.rows).fill('');
+    const cells: string[] = Array(props.rows*props.rows).fill('0');
     fillBombs(cells, props.bombs);
+    fillIndicators(cells, props.rows);
     const board: string[][] = [];
     for (let i = 0; i < props.rows; i++) {
         board.push(cells.slice(i,i+props.rows))
     }
-    console.log(board);
-    
     return (
         <div>
             <h1>Board</h1>
@@ -43,7 +132,7 @@ const Board = (props: BoardProps) => {
                     board.map((row) => (
                         <div className="row">
                             {row.map(cell => (
-                                <div>{cell ? cell : '🚩'}</div>
+                                <div>{cell}</div>
                             ))}    
                         </div>
                     ))
